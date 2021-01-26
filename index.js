5064a2f84b6129e3b3633dcd8fa2111760bb7d9c
Jung Lee <koreanstig@gmail.com>
2021-01-25 17:22:34 -0800

    added icons and added shading

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 var dayandtime = $("#currentDay").text(moment().format('MMMM Do YYYY'));
 var hour = moment().format('h');
 
+var descriptionFromStorage = JSON.parse(localStorage.getItem("text"));
+if (!descriptionFromStorage){
+    descriptionFromStorage = [];
+}
+for (var i = 0; i < descriptionFromStorage.length; i++){
+    var paragraph = document.createElement("p");
+    paragraph.innerText = scoresFromStorage[i].name + "'s score is " + scoresFromStorage[i].score;
+    document.body.appendChild(paragraph);
+}
+var storeButton = document.getElementById("store");
+var DescInput =  $(".description");
+var scoreInput =  $(".btn");
+storeButton.addEventListener("click", function(){
+    var name = nameInput.value;
+    var score = scoreInput.value;
+    var newLocalStorageEntry = {
+        name: name,
+        score: score
+    };
+    descriptionFromStorage.push(newLocalStorageEntry);
+    localStorage.setItem("text", JSON.stringify(descriptionFromStorage));
+})
