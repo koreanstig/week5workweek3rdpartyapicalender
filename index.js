7a4dcb50a7be87a9fad4fc170be55fd859fe0ad9
Jung Lee <koreanstig@gmail.com>
2021-01-27 14:33:12 -0600

    commented out everything while I figure out how to keep text in value in input into local storage and refresh

--- index.js
+++ index.js
@@ -2,10 +2,45 @@
 var dayAndYear = $("#currentDay").text(moment().format('MMMM Do YYYY'));
 var hour = moment().format('HH');
 var num = $(".num");
-var eventInput = $(".eventInput");
+// the below variable is to select the input and button
+var eventInput = document.querySelector(".eventInput");
 var enterButton = $(".enter-button");
 
+// call function that will keep input on page upon refresh
+renderInput();
 
+// function for keeping input into localstorage and keeping it there when refreshing page
+function renderInput(){
+    var input = localStorage.getItem("input");
+    if (!input){
+        return;
+    } else {
+        eventInput.value = input;
+    }
+    // console.log(input);
+    // console.log(eventInput);
+}
+$('body').click(function(event){
+    if (event.target.parentElement.classList.contains("add-item")){
+        // prevent default up top to prevent dom from doing what it normally does
+        event.preventDefault();
+        var input = eventInput.value;
+        console.log(input);
+        localStorage.setItem("input", input);
+        renderInput();
+    }
+});
+// new code for localStorage
+// enterButton.click(function(event){
+//     var button = $(this);
+//     var key = button.parent().parent().parent().attr("id");
+//     console.log(key);
+//     var value = button.parent().parent().parent().children().attr('value');
+//     console.log(value);
+//     console.log(event);
+// })
+
+// code for color change on inputs
 // for (i=0; i<num.length; i++){
 //     if (hour == num[i]){
 //         console.log(num[i]);
@@ -16,26 +51,3 @@ var enterButton = $(".enter-button");
 //         eventInput.css("background-color", "lightgreen");
 //     }
 // }
-
-// function renderLastRegistered() {
-//     var input = localStorage.getItem("input");
-//     if (!input) {
-//       return;
-//     }
-//     eventInput.textContent = input;
-//   }
-
-// enterButton.click(function(event){
-//     event.preventDefault();
-//     var input = $("eventInput").value;
-//     localStorage.setItem("input", input);
-//     renderLastRegistered();
-// });
-
-// $('body').click(function(event){
-//     if (event.target.parentElement.classList.contains("add-item")){
-//         console.log("Click")
-//         $('input').value = 'Hello';
-//         console.log($('input'));
-//     }
-// });
